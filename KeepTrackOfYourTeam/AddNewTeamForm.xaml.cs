7d3e2eeeb81bba152cc0c034b4151ab46e613e5b
Raymond <rkok12@gmail.com>
2018-09-21 20:46:16 +0200

    First design

---
+++ KeepTrackOfYourTeam/AddNewTeamForm.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KeepTrackOfYourTeam
+{
+    /// <summary>
+    /// Interaction logic for AddNewTeamForm.xaml
+    /// </summary>
+    public partial class AddNewTeamForm : Window
+    {
+        public AddNewTeamForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddAPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            var addPlayer = new AddAPlayerForm();
+            addPlayer.ShowDialog();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ KeepTrackOfYourTeam/TeamController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepTrackOfYourTeam
+{
+    public abstract class Team
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Points { get; set; }
+
+        public int CoachId { get; set; }
+    }
+
+    public class Coach : Team
+    {
+        public int Id { get; set; }
+
+        public int YearsExperience { get; set; }
+
+        public int PersonId { get; set; }
+    }
+
+    public class Players : Team
+    {
+        public int Id { get; set; }
+
+        public string Positie { get; set; }
+
+        public int PersonId { get; set; }
+
+        public int TeamId { get; set; }
+    }
+
+    public class Person : Team
+    {
+        public int Id { get; set; }
+
+        public int PersonId { get; set; }
+        
+        public string LastName { get; set; }
+
+        public string Adres { get; set; }
+    }
+}
